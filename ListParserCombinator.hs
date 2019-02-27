e7d4982b7015bb70b640b400b964a092c5a30616
Maria Kuklina <mkdirwtf@gmail.com>
2019-02-27 23:51:15 +0300

    Implement `minimum element number` feature.

--- ListParserCombinator.hs
+++ ListParserCombinator.hs
@@ -1,4 +1,4 @@
-module ListParserCombinator where
+module ListParserCombinator (parseList) where
 
 import Control.Applicative (Alternative, empty, (<|>))
 import Combinators
@@ -9,13 +9,17 @@ parseList :: Parser tok elem  -> -- Parser for the element.
              Parser tok delim -> -- Parser for the delimeter.
              Parser tok lbr   -> -- Parser for the left brace.
              Parser tok rbr   -> -- Parser for the right brace.
-             Integer          -> -- Minimum number of elements in the list.
+             Int              -> -- Minimum number of elements in the list.
              Parser tok [elem]
-parseList elem delim lbr rbr num = lbr *> sepBy elem delim <* rbr
--- TODO: last parameter
+parseList elem delim lbr rbr num = Parser $ \s -> do
+    let Parser p = lbr *> sepBy elem delim <* rbr
+    (s', elems) <- p s
+    if length elems < num
+    then empty
+    else pure (s', elems)
 
 
-test =
+testSimple =
      isJust (runParser parseAs "(a)")
   && isJust (runParser parseAs "(a,a,a)")
   && isNothing (runParser parseAs "(a,)")
@@ -26,4 +30,31 @@ test =
     rbr = char ')'
     elems = char 'a'
     delim = char ','
+    parseAs = parseList elems delim lbr rbr 0
+
+testSimpleMin =
+     isNothing (runParser parseAs "(a)")
+  && isJust (runParser parseAs "(a,a,a)")
+  && isNothing (runParser parseAs "(a,)")
+  && isNothing (runParser parseAs "(b,)")
+  && isNothing (runParser parseAs "(a;)")
+  where
+    lbr = char '('
+    rbr = char ')'
+    elems = char 'a'
+    delim = char ','
+    parseAs = parseList elems delim lbr rbr 2
+
+
+testMore =
+     isJust (runParser parseAs "((a))")
+  && isJust (runParser parseAs "((a@@a@@a))")
+  && isNothing (runParser parseAs "((a@@))")
+  && isNothing (runParser parseAs "((b@@))")
+  && isNothing (runParser parseAs "((a;))")
+  where
+    lbr = string "(("
+    rbr = string "))"
+    elems = char 'a'
+    delim = string "@@"
     parseAs = parseList elems delim lbr rbr 0
