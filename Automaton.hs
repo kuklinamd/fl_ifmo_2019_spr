aea76959394b5bfd3528cb3e1ea854d7f38e72b5
Maria Kuklina <mkdirwtf@gmail.com>
2019-03-13 13:57:19 +0300

    Make functions nested again

--- Automaton.hs
+++ Automaton.hs
@@ -55,79 +55,79 @@ isMinimal (Automaton sig sts init term delta) = error "`isMinimal` is not implem
 -- * Delta function is defined on not-a-state or not-a-symbol-from-sigma
 parseAutomaton :: String -> Either ParseError (Maybe (Automaton String String))
 parseAutomaton s = checkAutomation <$> snd <$> (runParser parseLists s)
-  --where
-fst' :: Maybe ([a1], a2) -> Maybe a2
-fst' (Just ([], auto)) = Just auto
-fst' _                 = Nothing
+  where
+    fst' :: Maybe ([a1], a2) -> Maybe a2
+    fst' (Just ([], auto)) = Just auto
+    fst' _                 = Nothing
 
-checkAutomation (_, _, Nothing, _, _) = Nothing
-checkAutomation (sgs, sts, Just init, terms, delta) = 
-  let emptyStates = not . null $ sts
-      initIsState = init `Set.member` sts
-      termsAreStates = all (flip Set.member sts) terms
-      deltaEntryCorrect ((q,s), q2) = q `Set.member` sts && not (null $ q2 `Set.intersection` sts)
-                                     && if isJust s then (fromJust s) `Set.member` sgs else True
-      deltaCorrect = all deltaEntryCorrect $  Map.toList delta
-  in
-  if emptyStates && initIsState && termsAreStates && deltaCorrect
-  then 
-  Just $ Automaton sgs sts init terms delta
-  else Nothing
+    checkAutomation (_, _, Nothing, _, _) = Nothing
+    checkAutomation (sgs, sts, Just init, terms, delta) = 
+      let emptyStates = not . null $ sts
+          initIsState = init `Set.member` sts
+          termsAreStates = all (flip Set.member sts) terms
+          deltaEntryCorrect ((q,s), q2) = q `Set.member` sts && not (null $ q2 `Set.intersection` sts)
+                                         && if isJust s then (fromJust s) `Set.member` sgs else True
+          deltaCorrect = all deltaEntryCorrect $  Map.toList delta
+      in
+      if emptyStates && initIsState && termsAreStates && deltaCorrect
+      then 
+      Just $ Automaton sgs sts init terms delta
+      else Nothing
 
-toSigma  = Set.fromList
+    toSigma  = Set.fromList
 
-toStates = Set.fromList
+    toStates = Set.fromList
 
-toInitState (q:[]) = Just q
-toInitState _ = Nothing
+    toInitState (q:[]) = Just q
+    toInitState _ = Nothing
 
-toTermState = Set.fromList
+    toTermState = Set.fromList
 
-toDelta [] = Map.empty
-toDelta ss = toDelta' ((\xs@((a1, a2, _):_) -> (a1, a2, (\(_,_,b) -> b) <$> xs)) <$> groupBy (\(a1, a2,_) (b1, b2,_) -> a1 == b1 && a2 == b2) ss)
+    toDelta [] = Map.empty
+    toDelta ss = toDelta' ((\xs@((a1, a2, _):_) -> (a1, a2, (\(_,_,b) -> b) <$> xs)) <$> groupBy (\(a1, a2,_) (b1, b2,_) -> a1 == b1 && a2 == b2) ss)
 
-toDelta' [] = Map.empty
-toDelta' ((src,symb,dists):ss) | symb == "\\epsilon" = Map.insert (src,Nothing) (Set.fromList dists) $ toDelta' ss
-                               | otherwise = Map.insert (src, Just symb) (Set.fromList dists) $ toDelta' ss
+    toDelta' [] = Map.empty
+    toDelta' ((src,symb,dists):ss) | symb == "\\epsilon" = Map.insert (src,Nothing) (Set.fromList dists) $ toDelta' ss
+                                   | otherwise = Map.insert (src, Just symb) (Set.fromList dists) $ toDelta' ss
 
-parseLists = betweenSpaces $ do
-  symbList <- parseSymbolList 1
-  betweenSpaces $ char ','
-  stateList <- parseStateList
-  betweenSpaces $ char ','
-  startList <- parseStartList
-  betweenSpaces $ char ','
-  termList  <- parseTerminalList
-  betweenSpaces $ char ','
-  deltList  <- parseDeltaList
-  eof
-  return (toSigma symbList,
-          toStates stateList,
-          toInitState startList,
-          toTermState termList,
-          toDelta deltList)
+    parseLists = betweenSpaces $ do
+      symbList <- parseSymbolList 1
+      betweenSpaces $ char ','
+      stateList <- parseStateList
+      betweenSpaces $ char ','
+      startList <- parseStartList
+      betweenSpaces $ char ','
+      termList  <- parseTerminalList
+      betweenSpaces $ char ','
+      deltList  <- parseDeltaList
+      eof
+      return (toSigma symbList,
+              toStates stateList,
+              toInitState startList,
+              toTermState termList,
+              toDelta deltList)
 
-parseSymbolList = parseList (betweenSpaces (some symbol)) delim lbr rbr
+    parseSymbolList = parseList (betweenSpaces (some symbol)) delim lbr rbr
 
-parseStateList = parseSymbolList 1
-parseStartList = parseSymbolList 1
-parseTerminalList = parseSymbolList 0
+    parseStateList = parseSymbolList 1
+    parseStartList = parseSymbolList 1
+    parseTerminalList = parseSymbolList 0
 
-parseDeltaList = parseList parseTriple delim lbr rbr 0
+    parseDeltaList = parseList parseTriple delim lbr rbr 0
 
-parseTriple = char '(' *> do {
-  s1 <- betweenSpaces (some symbol);
-  betweenSpaces $ char ',';
-  symb <- betweenSpaces (string "\\epsilon" <|> some symbol);
-  betweenSpaces $ char ',';
-  s2 <- betweenSpaces (some symbol);
-  pure (s1, symb, s2)} <* char ')'
+    parseTriple = char '(' *> do {
+      s1 <- betweenSpaces (some symbol);
+      betweenSpaces $ char ',';
+      symb <- betweenSpaces (string "\\epsilon" <|> some symbol);
+      betweenSpaces $ char ',';
+      s2 <- betweenSpaces (some symbol);
+      pure (s1, symb, s2)} <* char ')'
 
 
-symbol = betweenSpaces $ orChar $ ['a' .. 'z'] ++ ['A' .. 'Z'] ++ ['0'..'9']
-delim  = betweenSpaces $ char ','
-lbr    = betweenSpaces $ char '<'
-rbr    = betweenSpaces $ char '>'
+    symbol = betweenSpaces $ orChar $ ['a' .. 'z'] ++ ['A' .. 'Z'] ++ ['0'..'9']
+    delim  = betweenSpaces $ char ','
+    lbr    = betweenSpaces $ char '<'
+    rbr    = betweenSpaces $ char '>'
 
 testD = "<a,b,c,d>,<1,2,3,4>,<1>,<3,4>,<(1,a,3),(2,c,4)>"
 testND = "<a,b,c,d>,<1,2,3,4>,<1>,<3,4>,<(1,a,3),(1,\\epsilon,4),(2,c,4)>"
