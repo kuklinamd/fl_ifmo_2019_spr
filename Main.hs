320b7bff0aab23674974ed5ebd0aff731037c889
Maria Kuklina <mkdirwtf@gmail.com>
2019-04-09 15:27:41 +0300

    Fix Main to be more honest.

--- Main.hs
+++ Main.hs
@@ -1,8 +1,10 @@
 module Main where
-
+    
 import System.Environment
 import Expression
 import Text.Printf
+import Data.Char
+import Data.List
 
 main :: IO ()
 main = do
@@ -10,9 +12,18 @@ main = do
   mapM_
     (\fileName -> do
         input <- readFile fileName
-        let a = parseExpression input
+        let a = parseExpression $ trim input
         putStrLn $ printf "Parsing %s\n" fileName
         putStrLn $ either id show a
         putStrLn ""
     )
     fileNames
+
+
+trim xs = dropSpaceTail "" $ dropWhile isSpace xs
+
+dropSpaceTail maybeStuff "" = ""
+dropSpaceTail maybeStuff (x:xs)
+        | isSpace x = dropSpaceTail (x:maybeStuff) xs
+        | null maybeStuff = x : dropSpaceTail "" xs
+        | otherwise       = reverse maybeStuff ++ x : dropSpaceTail "" xs
