6dde2cfee5112599a23720c8aa78ab6f4c091a49
Maria Kuklina <mkdirwtf@gmail.com>
2019-04-09 15:17:18 +0300

    Add tests

--- Expression.hs
+++ Expression.hs
@@ -1,4 +1,4 @@
-module Expression where
+module Expression (parseExpression) where
 
 import Text.Printf
 import Combinators
@@ -26,7 +26,7 @@ data EAst a = BinOp Operator (EAst a) (EAst a)
 
 -- Change the signature if necessary
 parseExpression :: String -> Either ParseError (EAst Integer)
-parseExpression input = snd <$> runParser exprP input
+parseExpression input = snd <$> runParser (exprP <* eof) input
 
 binOp = flip BinOp
 
@@ -141,6 +141,7 @@ instance Show a => Show (EAst a) where
                   Primary x -> show x)
       ident = (+1)
 
+a = (BinOp Conj (BinOp Pow (Primary 1) (BinOp Sum (Primary 2) (Primary 3))) (Primary 4))
 
 {-
 show (BinOp Conj (BinOp Pow (Primary 1) (BinOp Sum (Primary 2) (Primary 3))) (Primary 4))
@@ -154,6 +155,23 @@ show (BinOp Conj (BinOp Pow (Primary 1) (BinOp Sum (Primary 2) (Primary 3))) (Pr
 |_4
 -}
 
+eval (Primary a) = a
+eval (BinOp op e1 e2) = applyOp op (eval e1) (eval e2)
+
+applyOp Pow i1 i2 = i1 ^ i2
+applyOp Mul i1 i2 = i1 * i2
+applyOp Div i1 i2 = i1 `div` i2
+applyOp Sum i1 i2 = i1 + i2
+applyOp Minus i1 i2 = i1 - i2
+applyOp Eq i1 i2   = toInteger $ fromEnum $ i1  == i2
+applyOp Neq i1 i2  = toInteger $ fromEnum $ i1 /= i2
+applyOp Le i1 i2   = toInteger $ fromEnum $ i1 <= i2
+applyOp Lt i1 i2   = toInteger $ fromEnum $ i1 < i2
+applyOp Ge i1 i2   = toInteger $ fromEnum $ i1 >= i2
+applyOp Gt i1 i2   = toInteger $ fromEnum $ i1 > i2
+applyOp Conj i1 i2 = toInteger $ fromEnum $ (1 == i1) && (1 == i2)
+applyOp Disj i1 i2 = toInteger $ fromEnum $ (1 == i1) || (1 == i2)
+
 cmpPR (Right (_, ok)) c b = (ok == c) == b
 cmpPR (Left _) _ _ = False
 
@@ -162,3 +180,19 @@ testParseEq = cmpPR (runParser cmpOp "==") Eq True
            && cmpPR (runParser cmpOp "<" ) Lt True
            && cmpPR (runParser cmpOp ">=") Ge True
            && cmpPR (runParser cmpOp ">" ) Gt True
+test =
+    let Right a1 = parseExpression "1+2+3"
+        test1 = eval a1 == 6
+        Right a2 = parseExpression "1+2*3"
+        test2 = eval a2 == 7
+        Right a3 = parseExpression "1*2+3"
+        test3 = eval a3 == 5
+        Right a4 = parseExpression "1^2^3"
+        test4 = eval a4 == 1
+        Right a5 = parseExpression "1&&0||1&&1"
+        test5 = eval a5 == 1
+        Right a6 = parseExpression "1||0&&0||1"
+        test6 = eval a6 == 1
+        Right a7 = parseExpression "10>4||10>=10"
+        test7 = eval a7 == 1
+    in and [test1, test2, test3, test4, test5, test6,test7]
