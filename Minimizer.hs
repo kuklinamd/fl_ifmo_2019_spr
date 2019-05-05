90198692daac5b08f4a97e05a81936f3652da6e3
Maria Kuklina <mkdirwtf@gmail.com>
2019-05-05 17:17:31 +0300

    Fix minimizer

--- Minimizer.hs
+++ Minimizer.hs
@@ -1,13 +1,17 @@
-module Minimizer (isMinimal, minimize) where
+module Minimizer {-(isMinimal, minimize)-} where
     
 import AutomatonType
-import Determiner (isDFA)
+import Determiner 
 
-import qualified Data.Set as Set
+import Data.Maybe (fromJust)
+
+import qualified Data.Set as Set hiding (unions)
 import qualified Data.Map.Strict as Map
 import qualified Data.Sequence as Seq
 
 import qualified Data.List as List
+
+import Data.Function (on)
 
 import Control.Applicative (liftA2)
 
@@ -16,10 +20,10 @@ type Seq = Seq.Seq
 -- Checks if the automaton is minimal (only for DFAs: the number of states is minimal)
 isMinimal :: (Ord a, Ord b) => Automaton a b -> Bool
 isMinimal a | not (isDFA a) = False
-isMinimal a = null $ findEqual a
+isMinimal a = null $ findEqual' a
 
 minimize :: (Ord q, Ord s) => Automaton s [q] -> Automaton s [q]
-minimize a | isMinimal a = a
+minimize a | not (isDFA a)  = minimize $ determine a
 minimize a = toSimple (minimizeCommon a)
   where
     toSimple (Automaton sig sts init term dlt) = Automaton sig sts' init' term' dlt'
@@ -40,7 +44,7 @@ toSingleState Bot = Bot
 minimizeCommon :: (Ord q, Ord s) => Automaton s q -> Automaton s (Set (State q))
 minimizeCommon a@(Automaton sig st init term dlt) = let
     -- List of sets, that contains equal states.
-    eq = findEqual a
+    eq = Set.toList $ equivClass $ Set.toList $ findEqual' a --findEqual a
     -- Set of not equals states.
     rest = restStates st eq
     lrest = Set.singleton <$> Set.toList rest
@@ -51,8 +55,7 @@ minimizeCommon a@(Automaton sig st init term dlt) = let
     newTerm   = Set.singleton (State term)
   in Automaton sig (Set.fromList $ State <$> newStates) newInit newTerm (newDelta dlt newStates newStates Map.empty)
   where
-    restStates st [] = st
-    restStates st (e:eq) = restStates (st Set.\\ e) eq
+    restStates st eq = st Set.\\ unions (Set.fromList eq)
 
     newInitSet [] = Set.singleton init
     newInitSet (s:[]) = s
@@ -60,7 +63,7 @@ minimizeCommon a@(Automaton sig st init term dlt) = let
 
     newDelta dlt st [] mp = mp
     newDelta dlt st (s:sts) mp = let
-        trans = map (\k -> Map.filterWithKey (findPartKey k)  dlt) $ Set.toList s
+        trans = map (\k -> Map.filterWithKey (stateMatches k)  dlt) $ Set.toList s
         in newDelta dlt st sts $ foldr (\t mp -> f mp (Map.toList t)) mp trans
         where
           f mp [] = mp
@@ -73,116 +76,96 @@ minimizeCommon a@(Automaton sig st init term dlt) = let
     findNewState (s:st) s2 | s2 `Set.member` s = s
     findNewState (s:st) s2 = findNewState st s2
 
-findEqual :: (Ord q, Ord s) => Automaton s q -> [Set (State q)]
-findEqual a = toSet (fst <$> Map.toList (Map.filter (\a -> not a) $ go tabl que))
+-- Reverse Delta ->
+-- Terminal States ->
+-- Sigs ->
+-- Init set ->
+-- List of set of equal states
+--findEqual' :: (Ord q, Ord s) => Delta q s -> Seq q -> Set s -> Set (q, q) -> [Set q]
+findDiffering rdlt term sigma qs = findDiffering' (Set.fromList qs) qs
   where
-    toSet l = toSet' ((\(a1, a2) -> Set.fromList [a1, a2]) <$> l)
+    findDiffering' m [] = m
+    findDiffering' marked ((q1,q2):qs) =
+      uncurry findDiffering' $ Set.foldr f (marked, qs) sigma
+      where
 
-    toSet' [] = []
-    toSet' b@(s:ss) = let a = uniteSets s ss in if (List.sort a) == (List.sort b) then a else toSet' a
+        -- Symbol -> (Marked, Queue) -> (Marked, Queue)
+        f symb (marked,qs) = foldr p (marked, qs) $ filter (uncurry (<)) $ all_pairs rs ss
+          where
+            -- List of states to go to from q1 and q2.
+            rs = maybe [] Set.toList $ Map.lookup (q1, Just symb) rdlt
+            ss = maybe [] Set.toList $ Map.lookup (q2, Just symb) rdlt
+        -- State -> State -> (Marked, Queue) -> (Marked, Queue)
+        p pr (mrkd, que) =
+          if pr `Set.member` mrkd
+            then (mrkd, que)
+            else (Set.insert pr mrkd, pr:que)
 
-    uniteSets s [] = [s]
-    uniteSets s (s':ss) | not (null $ Set.intersection s s') = uniteSets (Set.union s s') ss
-    uniteSets s (s':ss) = s' : uniteSets s ss
-    
-    revD = revDelta (states a) (delta a)
+findEqual' :: (Ord s, Ord q) => Automaton s q -> Set.Set (State q, State q)
+findEqual' a | not (isDFA a) = error "Not a DFA!"
+findEqual' a = let rdlt = revDelta (states a) (delta a)
+                   term = termState a
+                   sig  = sigma a
+                   qs = initTable' (states a) term
+                   st = Set.toList $ states a
+                in (Set.fromList $ filter (uncurry (<)) $ all_pairs st st)
+                    Set.\\ findDiffering rdlt term sig qs
 
-    (tabl, que) = initTable (states a) (termState a)
+setGroupBy :: Ord a => (a -> a -> Bool) -> Set a -> Set (Set a)
+setGroupBy p = uncurry put . Set.foldr f ([], Set.empty)
+  where f el ([], ac) = ([el], ac)
+        f el ((x:xs), ac) | p el x = (el:x:xs, ac)
+                          | otherwise = ([el], put (x:xs) ac)
+        put ls ac = Set.insert (Set.fromList ls) ac
 
-    go tabl que | Just (q1, q2) <- que Seq.!? 0 = let
-        -- Maps with symbols and transitions.
-        -- We need transitions: [q]
-        rs = (\((_, Just a), s) -> (a, Set.toList s)) <$> getNextStates q1
-        ss = (\((_, Just a), s) -> (a, Set.toList s)) <$> getNextStates q2
-
-        p ((s,ls):rs) ss | Just ls' <- lookup s ss = (s, uniq ls ls') : p rs ss
-        p [] ss = []
-        p (_:rs) ss = p rs ss
-
-        prs = p rs ss
-
-        newTabl = markTable tabl prs
-        newQue = updateQue (mapDiff tabl newTabl) (Seq.drop 1 que)
-
-        in go newTabl newQue
-    go tabl _ = tabl
-
-    uniq l1 l2 = filter (\p -> fst p /= snd p) ((,) <$> l1 <*> l2)
-
-    getNextStates q = Map.toList (Map.filterWithKey (findPartKey q) revD)
-
-     --where
-
-    markTable tabl [] = tabl
-    markTable tabl ((symb, sts):ss) = markTable (markTable' tabl sts) ss
-
-    markTable' tabl [] = tabl
-    markTable' tabl ((r,s):rss) | r /= s = markTable' (Map.alter func keyR (Map.alter func key tabl)) rss
-     where
-       key  = (r, s)
-       keyR = (s, r)
-
-       func _ = Just True
-    markTable' tabl (_:rs) = markTable' tabl rs
-
-    mapDiff = Map.differenceWith (\a b -> if a == False && b == True then Just b else Nothing)
-
-    -- In t only True
-    updateQue t q = foldr (\a b -> b Seq.|> a) q (fst <$> Map.toList t)
-
-
-initTable :: (Ord q) => Set q -> Set q -> (Map (q, q) Bool, Seq (q,q))
-initTable sts term = (,) (Map.fromList tabl) (Seq.fromList queue)
+equivClass :: Ord a => [(a, a)] -> Set (Set a)
+equivClass = equivClass' Set.empty
   where
-    tabl = markDiff term sts
-    queue = fst <$> filter snd tabl
+    equivClass' eqc [] = eqc
+    equivClass' eqc ((a, b):rs) = let
+      (ain, other) = Set.partition (Set.member a) eqc
+      (bin, other') = Set.partition (Set.member b) other
+      neqc = Set.insert a $ Set.insert b $ Set.union (unions ain) (unions bin)
+      in equivClass' (Set.insert neqc other') rs
 
-    xor True True = False
-    xor False False = False
-    xor _ _ = True
-    isTerm term q1 q2 = let b1 = Set.member q1 term
-                            b2 = Set.member q2 term
-                        in xor b1 b2
-    markDiff term s1 = (\q@(q1, q2) -> if isTerm term q1 q2 then (q, True) else (q, False)) <$> allStates s1
-    allStates s1 = filter (uncurry (/=)) $ allWithAll s1 s1
-    allWithAll s1 s2 = liftA2 (,) (Set.toList s1) (Set.toList s2)
+
+initTable' :: (Ord q) => Set q -> Set q -> [(q, q)]
+initTable' qs term =
+  let ql = Set.toList qs
+      allPairs = (,) <$> ql <*> ql
+      unorderedPairs = filter (uncurry (<)) allPairs
+   in filter (uncurry ((/=) `on` (\q -> Set.member q term))) unorderedPairs
+
+all_pairs :: Eq a => [a] -> [a] -> [(a, a)]
+all_pairs a b = filter (uncurry (/=)) $ (,) <$> a <*> b
 
 reachable :: (Ord q, Ord s) => q -> Delta q s -> Set q
-reachable _ dlt | Map.null dlt = Set.empty
-reachable start dlt = Set.insert start $ Set.unions (go <$> allTrans)
+reachable start dlt = Map.foldr (Set.union . go) (Set.singleton start) walked
   where
-    allTrans = Map.toList $ Map.filterWithKey (findPartKey start) dlt
-    newDlt = deleteKeys dlt $ fst <$> allTrans
+    (walked, unwalked) = Map.partitionWithKey (stateMatches start) dlt
+    go = unions . Set.map (\ntost -> reachable ntost unwalked)
 
-    go (_, tost) | Set.null tost = Set.empty
-    go (_, tost) = reachable (Set.elemAt 0 tost) newDlt
-
-    deleteKeys mp [] = mp
-    deleteKeys mp (k:ks) = deleteKeys (Map.delete k mp) ks
+unions :: Ord a => Set.Set (Set.Set a) -> Set.Set a
+unions = Set.foldr Set.union Set.empty
 
 -- Find all transitions from `st` state with each symbol.
-findPartKey st (k, _) _ | st == k = True
-findPartKey _ _ _ = False
+stateMatches st (k, _) _ = st == k
 
 -- For each state we look where it leads by each symbol
 -- and insert these values to our new delta.
 revDelta :: (Ord q, Ord s) => Set q -> Delta q s -> Delta q s
 revDelta sts dlt = Set.foldr f Map.empty sts
   where
-      -- Find all states from q and add them at rev delta.
-      --f :: q -> Delta q s -> Delta q s
-      f st revDlt = g
-        where
-            -- Find states and symbols from q.
-            m = Map.filterWithKey (findPartKey st) dlt
-            g = foldr h revDlt (Map.toList m)
+    -- Find all states from q and add them at rev delta.
+    --f :: q -> Delta q s -> Delta q s
+    f st revDlt = Map.foldrWithKey h revDlt (m st)
 
-            -- As FSM must be deterministic, there's only one new state
-            -- and no epsilon (Nothing) symbols.
-            h ((stt, Just symb), newsts) mp | Set.size newsts == 1 = Map.alter func key mp
-              where
-                newst = Set.elemAt 0 newsts
-                key = (newst, Just symb)
-                func (Just a) = Just (Set.insert stt a)
-                func Nothing = Just (Set.singleton stt)
-            h _ _ = error "Bad automaton input"
+    -- Find states and symbols from q.
+    m q = Map.filterWithKey (stateMatches q) dlt
+
+    -- As FSM must be deterministic, there's only one new state
+    h (stt, symb) newsts | [newst] <- Set.toList newsts =
+        let key = (newst, symb)
+            initOrInsert = maybe (Set.singleton stt) (Set.insert stt)
+        in Map.alter (Just . initOrInsert) key
+    h _ _ = error "Bad automaton input"
